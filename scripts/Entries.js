ae9fbe63f84baf66b514f8f1432bf863539fcc54
Jonathan Watson <thejmdw@gmail.com>
2021-04-20 21:58:12 -0400

    entries are rendering to DOM

---
+++ scripts/Entries.js
@@ -0,0 +1,18 @@
+import { getJournalEntries } from "./database.js"
+
+export const Entries = () => {
+  const entries = getJournalEntries() 
+  let allEntriesAsHTML = ""
+  console.log(entries)
+  for (const entry of entries) {
+
+    allEntriesAsHTML += `
+    <h3>${entry.concept}</h3>
+    <div>${entry.journalEntry}</div>
+    <div>${entry.journalDate}</div>
+    `
+    
+  }
+  
+  return allEntriesAsHTML
+}
